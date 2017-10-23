7264bf7b7beb6e127357cac0ad93ba1222412d4a
Trever Johnston <treverj1029@gmail.com>
2017-10-23 15:09:52 -0600

    added heroku to whitelist

--- server/config/handlers.js
+++ server/config/handlers.js
@@ -22,7 +22,7 @@ let defaultErrorHandler = (err, req, res, next) => {
 }
 
 //Default Urls 
-var whitelist = ['http://localhost:8080'];//add heroku website later
+var whitelist = ['http://localhost:8080', 'https://inspireq.herokuapp.com'];//add heroku website later
 var corsOptions = {
 	origin: function (origin, callback) {
 		var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
